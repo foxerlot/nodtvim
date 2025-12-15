37297e3393c83ffc6f63fb03b17c3ea8a84de621
Gregory Palamas <95938153+foxerlot@users.noreply.github.com>
2025-12-15 13:48:24 -0800

    setup VimEnter to open the splash-screen

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,8 +1,7 @@
 vim.api.nvim_create_autocmd("VimEnter", {
     callback = function()
         if vim.fn.argv(0) == "" then
-            require('files').open()
-            vim.api.nvim_buf_delete(1, { force = true })
+                require('splash').setScreen()
         end
     end
 })
